bc125841dbb4cbfaf09d35b2868be84b221bc1d3
Rouncer27 <trevor@switchbackcreative.ca>
2022-03-21 08:59:29 -0600

    added final SEO points

--- src/components/FormParts/functions/submitToServer.js
+++ src/components/FormParts/functions/submitToServer.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const submitToWebServer = async (formID, data) => {
-  const FORM_POST_URL = `${process.env.GATSBY_API_URL}/${formID}/feedback`
+  const FORM_POST_URL = `${process.env.GATSBY_CONTACT_FORM}/${formID}/feedback`
   const config = { headers: { "Content-Type": "multipart/form-data" } }
   const serverResponse = await axios.post(FORM_POST_URL, data, config)
 
